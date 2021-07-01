7319be8f338027e02c9465b68498ce32980a2090
Esdras Santos <esdrassantos41@gmail.com>
2021-07-01 20:43:58 -0300

    adicionando multiplicar,subtrair,dividir.js

---
+++ dividir.js
@@ -0,0 +1,8 @@
+function dividir(a,b){
+    if(a == 0 || b == 0){
+         return console.log("Não se pode dividir por zero")
+     } else {
+         return a / b 
+     }
+}
+module.exports = dividir
---
+++ multiplicar.js
@@ -0,0 +1,9 @@
+function multiplicar(a,b){
+    if(a == 0 || b == 0){
+        return console.log("TODO NUMERO MULTIPLICADO POR '0' É " + 0)
+    } else {
+        return a * b
+    }
+}
+module.exports = multiplicar
+
