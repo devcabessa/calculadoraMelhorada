fafc58065f741f0703d48748ee4c7b4388ae0c35
Esdras Santos <esdrassantos41@gmail.com>
2021-07-01 22:11:11 -0300

    arquivo calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+const somar = require("./somar")
+const subtrair = require("./subtrair")
+const multiplicar = require("./multiplicar")
+const dividir = require("./dividir")
+
+let resultado = somar(3,7)
+console.log(resultado)
+subtrair()
+multiplicar()
+dividir()
+
+
